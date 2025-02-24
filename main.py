f5a4b1c7da3dea7cb9dec8fc3426f3a96f331ba0
Jlevesque07 <joshual251@deltalearns.ca>
2025-02-24 16:56:41 +0000

    Save 1

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+shift = int(input("Shift"))
+word = input("word")
+wordlist = list(word)
+newlist = []
 
+for letter in wordlist:
+    newlist.append(chr(ord(letter) + shift))
+
+print(newlist)
+
+oglist = []
+
+for newletter in newlist:
+    oglist.append(chr(ord(newletter) - shift))
+
+print(oglist)
